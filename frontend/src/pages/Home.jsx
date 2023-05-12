2a7dedc6cdf0cf09f484b3c859fa801565c57da0
Ashin Amanulla <ashin.jnv@gmail.com>
2023-05-12 12:13:30 +0530

    jg

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Container from 'react-bootstrap/esm/Container'
 import './Home.css'
-
+import Card from 'react-bootstrap/Card';
 
 export default function Home() {
   return (
@@ -10,6 +10,24 @@ export default function Home() {
 <Container>
 
     <h1 className='text-center py-5 my-5'> Notes   <i className="bi bi-plus-circle anchor1"></i></h1>
+
+
+    <div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <div className='d-flex justify-content-between'>
+        <Card.Link href="#"><i class="bi bi-pen"></i></Card.Link>
+        <Card.Link href="#"><i class="bi bi-archive"></i></Card.Link>
+        </div>
+      
+      </Card.Body>
+    </Card>
+    </div>
   
 
 </Container>
