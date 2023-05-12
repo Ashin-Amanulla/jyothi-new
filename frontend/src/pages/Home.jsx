0ff37b5856ecee8f80e551bb4f669e1faf400e4a
Ashin Amanulla <ashin.jnv@gmail.com>
2023-05-12 12:08:06 +0530

    jg

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Container from 'react-bootstrap/esm/Container'
+import './Home.css'
+
 
 export default function Home() {
   return (
     <div>
         
 <Container>
-    <h1 className='text-center py-5 my-5'> Notes</h1>
+
+    <h1 className='text-center py-5 my-5'> Notes   <i className="bi bi-plus-circle anchor1"></i></h1>
+  
+
 </Container>
 
 
